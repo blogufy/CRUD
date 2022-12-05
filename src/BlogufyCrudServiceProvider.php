70f80c9bf6362e571110bb02f50ffeacc3747fda
sosupp <prospersosu@gmail.com>
2022-12-05 13:21:43 +0100

    first commit

---
+++ src/BlogufyCrudServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Blogufy\Crud;
+
+use Illuminate\Support\ServiceProvider;
+
+class BloguFyCrudServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        
+    }
+}
